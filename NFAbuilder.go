dfb4856fcd19ed1b9eb26f700ca6893c05c1af32
Brian Doyle <briandoyle147@gmail.com>
2018-04-06 20:50:11 +0100

    Used infix to postfix function aswell and appears to work perfectly

--- NFAbuilder.go
+++ NFAbuilder.go
@@ -220,25 +220,25 @@ func postMatch(postfix string, s string) bool {
 func main() {
 	//inToPost test cases
 	//Answer: ab.c*
-	//fmt.Println("Infix: ", "a.b.c*")
-	//fmt.Println("Postfix: ", inToPost("a.b.c*"))
+	fmt.Println("Infix: ", "a.b.c*")
+	fmt.Println("Postfix: ", inToPost("a.b.c*"))
 
 	//Answer: abd|.*
-	//fmt.Println("Infix: ", "(a.(b|d))*")
-	//fmt.Println("postFix: ", inToPost("(a.(b|d))*"))
+	fmt.Println("Infix: ", "(a.(b|d))*")
+	fmt.Println("postFix: ", inToPost("(a.(b|d))*"))
 
 	//Answer: abd|.c*
-	//fmt.Println("Infix: ", "a.(b|d).c*")
-	//fmt.Println("postFix: ", inToPost("a.(b|d).c*"))
+	fmt.Println("Infix: ", "a.(b|d).c*")
+	fmt.Println("postFix: ", inToPost("a.(b|d).c*"))
 
 	//Answer: abb.+.c.
-	//fmt.Println("Infix: ", "a.(b.b)+.c")
-	//fmt.Println("postFix: ", inToPost("a.(b.b)+.c"))
+	fmt.Println("Infix: ", "a.(b.b)+.c")
+	fmt.Println("postFix: ", inToPost("a.(b.b)+.c"))
 
 	//regexToNFA test case
-	//nfa := regexToNFA("ab.c*|")
-	//fmt.Println(nfa)
+	nfa := regexToNFA("ab.c*|")
+	fmt.Println(nfa)
 
 	//postMatch test case
-	fmt.Println(postMatch("ab.c*|", "cccc"))
+	fmt.Println(postMatch(inToPost("a.b.c*"), "abccc"))
 }
