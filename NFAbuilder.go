b4292485a5257ff74e22b7a84a3961d81ebc28e5
Brian Doyle <briandoyle147@gmail.com>
2018-04-06 13:38:00 +0100

    Added inital layout and test cases for infix to postfix

--- NFAbuilder.go
+++ NFAbuilder.go
@@ -1,82 +1,29 @@
 package main
-+
-+import (
-+	"fmt"
-+)
-+
-+type state struct {
-+	symbol rune
-+	edge1  *state
-+	edge2  *state
-+}
-+
-+type nfa struct {
-+	initial *state
-+	accept  *state
-+}
-+
-+func poregtonfa(pofix string) *nfa {
-+	nfastack := []*nfa{}
-+
-+	for _, r := range pofix {
-+		switch r {
-+		case '.':
-+			//Created two pointers frag2 and frag1 which are
-+			//pointers to nfa fragments
-+			frag2 := nfastack[len(nfastack)-1]
-+			nfastack = nfastack[:len(nfastack)-1]
-+
-+			frag1 := nfastack[len(nfastack)-1]
-+			nfastack = nfastack[:len(nfastack)-1]
-+
-+			frag1.accept.edge1 = frag2.initial
-+
-+			//Pops two fragments off the stack, joins the accept
-+			//state of the fragment
-+			nfastack = append(nfastack, &nfa{initial: frag1.initial, accept: frag2.accept})
-+
-+		case '|':
-+
-+			//Created two pointers frag2 and frag1 which are
-+			//pointers to nfa fragments
-+			frag2 := nfastack[len(nfastack)-1]
-+			nfastack = nfastack[:len(nfastack)-1]
-+
-+			frag1 := nfastack[len(nfastack)-1]
-+			nfastack = nfastack[:len(nfastack)-1]
-+
-+			initial := state{edge1: frag1.initial, edge2: frag2.initial}
-+			accept := state{}
-+			frag1.accept.edge1 = &accept
-+			frag2.accept.edge1 = &accept
-+
-+			//Pops two fragments off the stack, joins the accept
-+			//state of the fragment
-+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
-+
-+		case '*':
-+			frag := nfastack[len(nfastack)-1]
-+			nfastack = nfastack[:len(nfastack)-1]
-+
-+			accept := state{}
-+			initial := state{edge1: frag.initial, edge2: &accept}
-+			frag.accept.edge1 = frag.initial
-+			frag.accept.edge2 = &accept
-+
-+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
-+
-+		default:
-+			accept := state{}
-+			initial := state{symbol: r, edge1: &accept}
-+
-+			nfastack = append(nfastack, &nfa{initial: &initial, accept: &accept})
-+		}
-+	}
-+
-+	return nfastack[0]
-+}
-+
-+func main() {
-+	nfa := poregtonfa("ab.c*|")
-+	fmt.Printf("%v", nfa)
-+}
+
+import (
+	"fmt"
+)
+
+func inToPost(infix string) string {
+	postfix := ""
+
+	return postfix
+}
+
+func main() {
+	//Answer: ab.c*
+	fmt.Println("Infix: ", "a.b.c*")
+	fmt.Println("Postfix: ", inToPost("a.b.c*"))
+
+	//Answer: abd|.*
+	fmt.Println("Infix: ", "(a.b|d))*")
+	fmt.Println("postFix: ", inToPost("(a.b|d))*"))
+
+	//Answer: abd|.c*
+	fmt.Println("Infix: ", "a.(b|d).c*")
+	fmt.Println("postFix: ", inToPost("a.(b|d).c*"))
+
+	//Answer: abb.+.c.
+	fmt.Println("Infix: ", "a.(b.b)+.c")
+	fmt.Println("postFix: ", inToPost("a.(b.b)+.c"))
+}
